a79f3a902b6a691d5083b2621aa98a91b583019d
astarinatyasr <asnasa@yahoo.com>
2015-10-06 17:23:12 +0700

    Update client.py

--- 2. Socket Communicating/client.py
+++ 2. Socket Communicating/client.py
@@ -1,38 +1,20 @@
+#!/usr/bin/python
+
 import socket
 
-HOST = '192.168.1.110'    #server name goes in here
-PORT = 11309             #port number goes in here
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((HOST,PORT))
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+host = socket.gethostname()
+port = 8888
+data = None
 
-
-msg = raw_input('Enter your name: ')
-while(1):
-    print 'Hello',
-    print msg,
-    print '!',
-    print 'Welcome to my page!'
-    print 'Click "1" to download and read the content of abc.txt '
-    print 'Click "2" to download and read the content of def.txt '
-    
-    x = raw_input('Enter your choice: ')
-    
-    if x == "1":
-        with open('myTransfer.txt', 'wb') as file_to_write:
-            while True:
-                data = socket.recv(1024)
-                print data
-                if not data:
-                    break
-                file_to_write.write(data)
-    elif x == "2":
-        with open('myTransfer.txt', 'wb') as file_to_write:
-            while True:
-                data = socket.recv(1024)
-                print data
-                if not data:
-                    break
-                file_to_write.write(data)
-    raise SystemExit
-socket.close()
-
+s.connect((host, port))
+print s.recv(1024)
+while (data != "exit"):
+        data = raw_input()
+        s.sendall(data)
+        if data == "exit" :
+                print '\n--exiting--\n'
+                break
+        print s.recv(1024)
+s.close
